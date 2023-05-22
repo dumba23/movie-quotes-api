cb80235d9076900ad61f84b9c4682826a2fb7ec0
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-05-22 15:48:05 +0400

    feat(register): add mail confirm functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Mail\MailConfirm;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
 class AuthController extends Controller
@@ -14,9 +16,11 @@ class AuthController extends Controller
 	{
 		$email_verify_token = Str::random(30);
 
-		User::create($request->validated() + [
+		$user = User::create($request->validated() + [
 			'email_verify_token' => $email_verify_token,
 		]);
+
+		Mail::to($user->email)->send(new MailConfirm($user));
 
 		return response()->json(['success' => true, 'token' => $email_verify_token], 201);
 	}
