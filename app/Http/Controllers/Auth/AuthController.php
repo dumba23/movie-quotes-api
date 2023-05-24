b909741c11aba3d45267d3bc95ea079137bfc789
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-05-24 14:23:29 +0400

    feat(login): create custom and google login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Mail\MailConfirm;
 use App\Models\User;
@@ -23,5 +24,35 @@ class AuthController extends Controller
 		Mail::to($user->email)->send(new MailConfirm($user));
 
 		return response()->json(['success' => true, 'token' => $email_verify_token], 201);
+	}
+
+	public function login(LoginRequest $request): JsonResponse
+	{
+		$login_type = filter_var($request->login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+		$remember = (bool)$request->validated('remember_token');
+
+		$request->merge([$login_type => $request->input('login')]);
+
+		$user = User::where($login_type, $request->$login_type)->first();
+
+		if ($user && $user->email_verified_at == null) {
+			return response()->json([
+				'status'  => false,
+				'message' => 'Email is not confirmed yet',
+			], 401);
+		}
+
+		if (!auth()->attempt($request->only($login_type, 'password'), $remember)) {
+			return response()->json([
+				'status'  => false,
+				'message' => 'Provided credentials does not match with our record.',
+			], 401);
+		}
+
+		return response()->json([
+			'status'  => true,
+			'message' => 'User Logged In Successfully',
+		], 200);
 	}
 }
