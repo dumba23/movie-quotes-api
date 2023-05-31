2a2ed91d2701ba1ad076266524b1190db61f772e
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-05-31 16:51:35 +0400

    refactor: fix url mismatches

--- resources/views/mail/confirm.blade.php
+++ resources/views/mail/confirm.blade.php
@@ -7,7 +7,7 @@
     <body style="width: 100%; height:100%; background: linear-gradient(187.16deg, #181623 100.07%, #191725 100.65%, #0D0B14 98.75%);">
         <div style="padding:140px; max-width:1229px; margin:0 auto;">
             <div style="text-align:center; margin-bottom: 56px;">
-                <img src="{{$message->embed(public_path() . '/images/Vector.png')}}" style="margin-top: 50px;" alt="Dashboard"/>
+                <img src="{{$message->embed(public_path() . '/images/IconMessage.png')}}" style="margin-top: 50px;" alt="Dashboard"/>
                 <p style="color: #DDCCAA">MOVIE QUOTES</p>
             </div>
             <div style="margin-bottom:40px; color: #ffffff;">
@@ -25,7 +25,7 @@
                 </a>
                 <p>If clicking doesn't work, you can try copying and pasting it to your browser:</p>
                 <div style="max-width:1200px;">
-                    <p style="word-break: break-all; color: #DDCCAA;">https://moviequotes/verify?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdHYiOjIsImp0aSI6ImRkNTVkMTM1LTFhMGEtNDBjNi04NDQ5LThmZjdkZGFlNDc3MyIsImVtYWlsVmVyaWZpZWQiOmZhbHNlLCJpYXQiOjE2MDE2NzIxMIyYjk2NDNhNjdjNjU1NmQ5MSJ9.ok1qXOM-RY9_x2_xKjDj2aK6NG9w9xAMcDpchUnbn4c</p>
+                    <p style="word-break: break-all; color: #DDCCAA;">{{ 'http://localhost:5173/?token=' . $user->email_verify_token }}</p>
                 </div>
                 <p>If you have any problems, please contact us: support@moviequotes.ge</p>
                 <p>MovieQuotes Crew</p>
