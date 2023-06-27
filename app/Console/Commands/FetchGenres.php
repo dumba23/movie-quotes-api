92dea23900fd2de0d723258f0152ded5025a0ef8
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-27 12:49:15 +0400

    refactor: extract api endpoint into env

--- app/Console/Commands/FetchGenres.php
+++ app/Console/Commands/FetchGenres.php
@@ -8,36 +8,36 @@ use Illuminate\Support\Facades\Http;
 
 class FetchGenres extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'fetch:genres';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'fetch:genres';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Fetches genres from themoviedb';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Fetches genres from themoviedb';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        $response = Http::get('https://api.themoviedb.org/3/genre/movie/list', [
-            'api_key' => env('GENRE_API_KEY'),
-            'language' => 'en-US',
-        ]);
+	/**
+	 * Execute the console command.
+	 */
+	public function handle()
+	{
+		$response = Http::get(env('GENRE_API_URL') . '/3/genre/movie/list', [
+			'api_key'  => env('GENRE_API_KEY'),
+			'language' => 'en-US',
+		]);
 
-        $genres = $response->json()['genres'];
+		$genres = $response->json()['genres'];
 
-        foreach ($genres as $genre) {
-            Genre::updateOrCreate(['name' => $genre['name']]);
-        }
+		foreach ($genres as $genre) {
+			Genre::updateOrCreate(['name' => $genre['name']]);
+		}
 
-        $this->info('Genres updated successfully.');
-    }
+		$this->info('Genres updated successfully.');
+	}
 }
