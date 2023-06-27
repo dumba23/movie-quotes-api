ba84052ba66e282126673d954a929b06625971f2
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-27 12:50:13 +0400

    feat: add fillable and translation properties to quote model

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Quote extends Model
 {
     use HasFactory;
+    use HasTranslations;
+
+    protected $fillable = [
+        'title_en',
+        'title_ka',
+        'image',
+        'movie_id',
+    ];
+
+    public $translatable = ['title'];
 
     public function movie()
     {
