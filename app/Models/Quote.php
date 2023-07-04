95423f470187e36d48ca2d2255a994e369c870e8
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-07-04 20:08:39 +0400

    feat: create and update models

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,24 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Translatable\HasTranslations;
 
 class Quote extends Model
 {
-    use HasFactory;
-    use HasTranslations;
+	use HasFactory;
 
-    protected $fillable = [
-        'title_en',
-        'title_ka',
-        'image',
-        'movie_id',
-    ];
+	use HasTranslations;
 
-    public $translatable = ['title'];
+	protected $fillable = [
+		'title_en',
+		'title_ka',
+		'image',
+		'movie_id',
+	];
 
-    public function movie()
-    {
-        return $this->belongsTo(Movie::class);
-    }
+	public $timestamps = true;
+
+	public $translatable = ['title'];
+
+	public function movie(): BelongsTo
+	{
+		return $this->belongsTo(Movie::class);
+	}
+
+	public function comments(): HasMany
+	{
+		return $this->hasMany(Comment::class);
+	}
+
+	public function likes(): BelongsToMany
+	{
+		return $this->belongsToMany(User::class, 'likes');
+	}
 }
